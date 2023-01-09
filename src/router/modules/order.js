acef63bb1fe5153a432cb06bb3502a7a9a7c2d8b
qinxianwu <qinxianwu@qq.com>
2023-01-09 16:54:40 +0800

    fix: 隐藏配送订单

--- src/router/modules/order.js
+++ src/router/modules/order.js
@@ -1,7 +1,7 @@
 // import PERMISSION_ID from "@/permission/PERMISSION_ID";
 import MainLayout from "@/layouts/MainLayout/index";
 import PurchaseOrder from "@/views/Order/views/Purchase/index.vue";
-import SendGoodsOrder from "@/views/Order/views/SendGoods/index.vue";
+// import SendGoodsOrder from "@/views/Order/views/SendGoods/index.vue";
 
 export default {
   path: "/Order",
@@ -24,14 +24,14 @@ export default {
         // PermissionId: PERMISSION_ID.Goods_List,
       },
     },
-    {
-      path: "SendGoodsOrder",
-      component: SendGoodsOrder,
-      name: "SendGoodsOrder",
-      meta: {
-        title: "配送订单",
-        // PermissionId: PERMISSION_ID.Goods_List,
-      },
-    },
+    // {
+    //   path: "SendGoodsOrder",
+    //   component: SendGoodsOrder,
+    //   name: "SendGoodsOrder",
+    //   meta: {
+    //     title: "配送订单",
+    //     // PermissionId: PERMISSION_ID.Goods_List,
+    //   },
+    // },
   ],
 };
